cb54bb43f21cbbe6ef3174a21e37e5f21a8ae32d
Erthium <ertyum35@gmail.com>
2024-02-10 18:23:43 +0300

    Remove default 'Hello' route and change it to connection validation

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,5 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { AiService } from './ai/ai.service';
 import { EditorController } from './editor/editor.controller';
 import { EditorService } from './editor/editor.service';
@@ -8,6 +7,6 @@ import { EditorService } from './editor/editor.service';
 @Module({
   imports: [],
   controllers: [AppController, EditorController],
-  providers: [AppService, AiService, EditorService],
+  providers: [AiService, EditorService],
 })
 export class AppModule {}
