9d104917661c7605294a924b976d0ce2b5e77f9d
Erthium <ertyum35@gmail.com>
2024-02-17 14:40:41 +0300

    Initialise firebase

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,12 +7,28 @@ import { EditorController } from './editor/editor.controller';
 import { EditorService } from './editor/editor.service';
 import { StorageService } from './storage/storage.service';
 import { IdentifierService } from './identifier/identifier.service';
+import { FirebaseService } from './firebase/firebase.service';
+
+import * as admin from 'firebase-admin';
+
+const serviceAccount = require('../serviceAccountKey.json');
+
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+    storageBucket: "gs://ai-summit-image-edit.appspot.com"
+});
 
 @Module({
     imports: [ConfigModule.forRoot({
         envFilePath: '.env',
     })],
     controllers: [AppController, EditorController],
-    providers: [AiService, EditorService, StorageService, IdentifierService],
+    providers: [
+        AiService, 
+        EditorService, 
+        StorageService, 
+        IdentifierService, 
+        FirebaseService
+    ],
 })
 export class AppModule {}
