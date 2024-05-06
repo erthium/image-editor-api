c7a580d28a1f64c32ebb7ade2a646c9b814d547d
Erthium <ertyum35@gmail.com>
2024-05-06 11:59:50 +0300

    fix: try different dimensions and offsets

--- src/storage/storage.service.ts
+++ src/storage/storage.service.ts
@@ -16,6 +16,7 @@ export class StorageService {
       if (framed) {
         // Load images
         const image_buf = Buffer.from(image64, 'base64');
+
         const frame_path = join(__dirname, '../assets/sw-frame.png');
         const frame_logo_path = join(__dirname, '../assets/frame_logo.png');
         const frame = sharp(frame_path);
@@ -23,7 +24,7 @@ export class StorageService {
 
         // Compose
         const result_buf = await frame.composite([
-          { input: image_buf, top: 30, left: 28},
+          { input: image_buf, top: 27, left: 26},
           { input: frame_logo }
         ]).toBuffer();
 
