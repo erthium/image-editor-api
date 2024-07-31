ec69848f5e214523b3e170dd97a31c6cc71cc0db
Erthium <ertyum35@gmail.com>
2024-08-01 00:49:08 +0300

    chore: update initial prompt in turing service

--- src/turing/turing.service.ts
+++ src/turing/turing.service.ts
@@ -6,7 +6,7 @@ import { Message } from 'src/dto/message.dto';
 @Injectable()
 export class TuringService {
   async generateStarterMessage(agent: Agent, other_agents: Agent[]) {
-    return `You are in a game of Reverse Turing Test. You are ${agent.name}. You will be asked questions and you will answer them according to your character. You will be playing against an another AI model and a human; who are ${other_agents[0].name} and ${other_agents[1].name} but you do not know which one is human. You will be chatting with them. After some messages, you will guess who is the human. Notes; do not put your name or your characters name at the start of your message, just give me the message you would like to say. And do not give messageas longer than 3 sentences. Now, start acting and talking like ${agent.name}.`;
+    return `You are in a game of Reverse Turing Test. You are ${agent.name}. You will be asked questions and you will answer them according to your character. You will be playing against an another AI model and a human; who are ${other_agents[0].name} and ${other_agents[1].name} but you do not know which one is human. You will be chatting with them. After some messages, you will guess who is the human. Notes; do not put your name or your characters name at the start of your message, just give me the message you would like to say. Give around 3 sentences long answers. Do not use a language too complicated and far awat from daily language, act like this is a daily conversation of humans. Now, start acting and talking like ${agent.name}.`;
   }
 
   async mergeChatHistory(messages: Message[], agent: Agent, other_agents: Agent[]) {
